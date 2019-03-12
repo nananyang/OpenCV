e208d087b012fd672cb1fd161a338ac2f14d18e8
YD <unlocks@nate.com>
2019-03-12 18:28:15 +0900

    2019-03-07

--- OpenCV/ocean.py
+++ OpenCV/ocean.py
@@ -6,10 +6,10 @@ while True:
     if(song.get(cv2.CAP_PROP_POS_FRAMES) == song.get(cv2.CAP_PROP_FRAME_COUNT)):
         song.open('TheOcean.mp4')
         
-        ret, frame = song.read()
-        cv2.imshow('VideoFrame', frame)
+    ret, frame = song.read()
+    cv2.imshow('VideoFrame', frame)
         
-        if cv2.waitKey(1) > 0: break
+    if cv2.waitKey(30) > 0: break
             
 song.release()
 cv2.destroyAllWindows()
